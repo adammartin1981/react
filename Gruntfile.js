c995e69a82b814a6c4a8d34351afd34f7fc23972
Adam Martin <amartin@quark.com>
2014-09-24 16:37:18 +0100

    #add - Express Web Server functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,65 @@
 module.exports = function(grunt) {
     // Do grunt-related things in here
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // Project configuration.
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
-            uglify: {
-                options: {
+
+        uglify: {
+            options: {
                 banner: '/*! This is the banner to display at the top <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+
+        // Grunt express - our webserver
+        // https://github.com/blai/grunt-express
+        express: {
+            all: {
+                options: {
+                    bases: ['./demo'],
+                    port: 8088,
+                    hostname: "localhost",
+                    livereload: true
+                }
+            }
+        },
+
+        // grunt-watch will monitor the projects files
+        // https://github.com/gruntjs/grunt-contrib-watch
+        watch: {
+            all: {
+                    files: '**/*.html',
+                    options: {
+                        livereload: true
+                }
+            }
+        },
+
+        // grunt-open will open your browser at the project's URL
+        // https://www.npmjs.org/package/grunt-open
+        open: {
+            all: {
+                path: 'http://localhost:8088/index.html'
+            }
         }
+
     });
 
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
+
+    grunt.registerTask('server', [
+        'express',
+        'open',
+        'watch'
+    ]);
 
 };
